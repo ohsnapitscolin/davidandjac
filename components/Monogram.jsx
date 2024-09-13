e669b0160ffe11ac83ea835f4f7b69069765d20e
Colin Dunn <colin@squidcloud.io>
2024-09-13 08:19:55 -0700

    updated transition

--- components/Monogram.jsx
+++ components/Monogram.jsx
@@ -4,7 +4,7 @@ import MonogramIcon from "@/assets/monogram.svg";
 
 const Monogram = ({ className, color, onClick }) => {
   const [nextColor, setNextColor] = useState();
-  const [enabled, setEnabled] = useState();
+  const [enabled, setEnabled] = useState(true);
 
   useEffect(() => {
     setEnabled(false);
@@ -16,15 +16,21 @@ const Monogram = ({ className, color, onClick }) => {
     }, 1000);
   }, [color]);
 
-  const { text, bg } = COLOR_MAP[color];
-  const { hoverText, hoverBg } = COLOR_MAP[nextColor] || {};
+  const { bg } = COLOR_MAP[color];
+  const { groupHoverBg } = COLOR_MAP[nextColor] || {};
 
   return (
     <button
-      className={`text-base rounded-full p-1 font-alpina hover:transition-colors ${text} ${bg} ${enabled && `${hoverText} ${hoverBg}`} ${className}`}
+      className={`relative group ${className}`}
       onClick={() => onClick(nextColor)}
       disabled={!enabled}
     >
+      <div
+        className={`absolute rounded-full top-3 left-3 ${bg} h-[62px] w-[124px] ${enabled && `group-hover:top-0 group-hover:left-0 group-hover:h-full group-hover:w-full ${groupHoverBg}`}`}
+        style={{
+          transition: "all 300ms cubic-bezier(0.5, 0, 0, 1)",
+        }}
+      />
       <MonogramIcon style={{ mixBlendMode: "overlay" }} />
     </button>
   );
--- constants.js
+++ constants.js
@@ -4,36 +4,42 @@ export const COLOR_MAP = {
     bg: "bg-orange-bg",
     hoverText: "md:hover:text-orange-text",
     hoverBg: "md:hover:bg-orange-bg",
+    groupHoverBg: "md:group-hover:bg-orange-bg",
   },
   blue: {
     text: "text-blue-text",
     bg: "bg-blue-bg",
     hoverText: "md:hover:text-blue-text",
     hoverBg: "md:hover:bg-blue-bg",
+    groupHoverBg: "md:group-hover:bg-blue-bg",
   },
   red: {
     text: "text-red-text",
     bg: "bg-red-bg",
     hoverText: "md:hover:text-red-text",
     hoverBg: "md:hover:bg-red-bg",
+    groupHoverBg: "md:group-hover:bg-red-bg",
   },
   green: {
     text: "text-green-text",
     bg: "bg-green-bg",
     hoverText: "md:hover:text-green-text",
     hoverBg: "md:hover:bg-green-bg",
+    groupHoverBg: "md:group-hover:bg-green-bg",
   },
   tan: {
     text: "text-tan-text",
     bg: "bg-tan-bg",
     hoverText: "md:hover:text-tan-text",
     hoverBg: "md:hover:bg-tan-bg",
+    groupHoverBg: "md:group-hover:bg-tan-bg",
   },
   brown: {
     text: "text-brown-text",
     bg: "bg-brown-bg",
     hoverText: "md:hover:text-brown-text",
     hoverBg: "md:hover:bg-brown-bg",
+    groupHoverBg: "md:group-hover:bg-brown-bg",
   },
 };
 
